cd92322e3a5d452f5ea285be84df8682b118fadf
stephweb <stephweb@live.fr>
2017-01-20 05:59:45 +0100

    Refacto comments

--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use Core\Controller\BaseController;
 
 /**
- * Gestion des articles
+ * Controlelr parent de l'application
  */
 class Controller extends BaseController
 {
